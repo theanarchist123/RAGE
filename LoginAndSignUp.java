f8358b21808b5151ad2a9ec18ed5cee203b1a9c1
theanarchist123 <2023.nikhil.kadam@ves.ac.in>
2024-10-05 18:36:37 +0530

    Add files via upload

---
+++ LoginAndSignUp.java
@@ -0,0 +1,14 @@
+
+package loginandsignup;
+
+public class LoginAndSignUp {
+
+    public static void main(String[] args) {
+        NewLogin Login1 = new NewLogin();
+        Login1.setVisible(true);
+        Login1.pack();
+        Login1.setLocationRelativeTo(null);
+
+    }
+    
+}
